0b83ccfd995bf8b4db4b6990bc8772e4fdb43693
oikiou <oikiou@yahoo.co.jp>
2017-12-05 15:19:15 +0900

    choose 完成

--- heat_exchange.py
+++ heat_exchange.py
@@ -6,7 +6,13 @@ import numpy as np
 ## 通过额定参数 计算盘管热通过有效度
 # 输入 定格 流体1入口温度 流量 流体2入口温度 流量 加热能力
 # 输出 热通过有效度
-def hx_e_e(t1, t2, f1, f2, power, c1, c2, rho1, rho2):
+def hx_e_e(t1, t2, f1, f2, power, l1, l2):
+    dic = {"water":[4186, 1000],
+           "air":[1005, 1.2]}  # 比热 密度
+    c1 = dic[l1][0]  # [J/kg.K]
+    c2 = dic[l2][0]
+    rho1 = dic[l1][1]  # [kg/m3]
+    rho2 = dic[l2][1]
     G1 = rho1 * f1  # [kg/s]
     G2 = rho2 * f2
     cG1 = c1 * G1  # [W/K]
@@ -20,11 +26,7 @@ t2 = 22
 f1 = 0.004/60  # [m3/s]
 f2 = 340/3600  # [m3/s]
 power = 3200  # [w]
-c1 = 4186  # [J/kg.K]
-c2 = 1005
-rho1 = 1000  # [kg/m3]
-rho2 = 1.2
-print(hx_e_e(t1,t2,f1,f2,power,c1,c2,rho1,rho2))
+print(hx_e_e(t1,t2,f1,f2,power, l1="water", l2="air"))
 
 ## 盘管实际出口温度计算
 # 输入 流体1入口温度 流量 比热 流体2入口温度 流量 比热 热通过有效度
