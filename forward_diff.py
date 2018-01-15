755d7f9c20209d90e7b2724899df91b0d61f4632
oikiou <oikiou@yahoo.co.jp>
2018-01-15 11:24:27 +0900

    forward_diff_v0.91

--- forward_diff.py
+++ forward_diff.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
 
@@ -12,13 +13,14 @@ def diff_m(d, m, dt, indoor_t, outdoor_t, time, _lambda, _c_rho):
     t_out = []
     t_old_m = [0] * (m + 1)  # 初始条件
     for i in range(int(time / dt)):
-        t_old_l = [indoor_t] + list(t_old_m[:-1])  # 边界条件
-        t_old_r = list(t_old_m[1:]) + [outdoor_t]  # 边界条件
+        t_old_l = [indoor_t] + list(t_old_m[:-1])
+        t_old_r = list(t_old_m[1:]) + [outdoor_t]
         t_new = np.multiply(ul, t_old_l) + np.multiply(um, t_old_m) + np.multiply(ur, t_old_r)
         t_out.extend(list(t_new))
         t_old_m = t_new
-    print(np.array(t_out).reshape((-1, m + 1)))
+    print(np.array(t_out).reshape(-1, m + 1))
+    plt.plot(np.array(t_out).reshape(-1, m + 1))
+    plt.show()
 
 # 厚度，网格数，时间间隔，室温，外气温度，计算时长，传热系数，质量比热
-diff_m(0.2, 3, 60, 26, 0, 1200, 1.4, 1934)
-
+diff_m(0.2, 3, 60, 26, 0, 3600, 1.4, 1934)
