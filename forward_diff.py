2db4bb3d3d02a52ab267921f98a530fb818ad133
oikiou <oikiou@yahoo.co.jp>
2018-01-17 22:42:07 +0900

    debug

--- forward_diff.py
+++ forward_diff.py
@@ -5,8 +5,8 @@ np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
 
 def diff_m(d, m, dt, indoor_t, outdoor_t, time, _lambda, _c_rho):
-    r = [1/9] + [d/m/_lambda] * m + [1/23]
-    cap = [0] + [_c_rho/d*m] * m + [0]
+    r = [1/9] + [d/m/_lambda] * m + [1/9]
+    cap = [0] + [_c_rho*d/m*1000] * m + [0]
     ul = [dt / 0.5 / (cap[i] + cap[i + 1]) / r[i] for i in range(m + 1)]
     ur = [dt / 0.5 / (cap[i] + cap[i + 1]) / r[i + 1] for i in range(m + 1)]
     um = np.multiply(np.add(ul, ur), -1) + 1
@@ -24,3 +24,4 @@ def diff_m(d, m, dt, indoor_t, outdoor_t, time, _lambda, _c_rho):
 
 # 厚度，网格数，时间间隔，室温，外气温度，计算时长，传热系数，质量比热
 diff_m(0.2, 3, 60, 26, 0, 3600, 1.4, 1934)
+#diff_m(1, 10, 120, 25, 10, 1200, 1.4, 1934)
